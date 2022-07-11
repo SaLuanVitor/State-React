d56a5e04981acc9c48f1b9d829859242142d409b
Luan Vitor <luanvitors_852@hotmail.com>
2022-07-10 22:20:55 -0300

    restante dos arquivos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import './App.css';
+import Abacate from "./Assets/Abacate.jpg"
+
+
+
+class App extends Component {
+  state = {
+    nome: "Luan",
+    idade: 22,
+    comidaFavorita: "Torta de Frango",
+    musicas: ["É osso", "Love Love", "Pontos de exclamação"]
+  }
+  render() {
+    return (
+      <>
+        <h1>Me chamo: {this.state.nome}</h1>
+        <h2>Tenho {this.state.idade} anos</h2>
+        <h3>Gosto de comer {this.state.comidaFavorita}</h3>
+        <h3>Músicas:</h3>
+        <ul>
+          <li>{this.state.musicas[0]}</li>
+          <li>{this.state.musicas[1]}</li>
+          <li>{this.state.musicas[2]}</li>
+        </ul>
+        <h3>Uma das minhas frutas prediletas:</h3>
+        <img src={Abacate}></img>
+      </>
+    );
+  }
 }
 
 export default App;
